3b69cb57b164a8e1f2398e800693a98c410bcb3f
Hafif Rahman <hafif0897@gmail.com>
2026-04-30 23:02:15 +0800

    refactor: move prisma database config to env variables

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,10 +3,10 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '@/prisma/generated/client'
 
 const adapter = new PrismaMariaDb({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'percetakan_app1',
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
 })
 
 export const prisma = new PrismaClient({ adapter })
